dcb19f953cee333bfd50ed3bbd1498ae689b9142
benquang2000vn <140497141+benquang2000vn@users.noreply.github.com>
2023-07-25 16:10:45 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker"],
+ typeSpeed:10,
+ backSpeed:100,
+ loop:true,
+ });
+ $('.counter').countUp();
+
+ new WOW().init();
